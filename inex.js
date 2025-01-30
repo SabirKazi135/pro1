a8691dbdde0f14e2d0360856c098e4c3b74e98d4
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-30 21:38:03 +0530

    commit

--- inex.js
+++ inex.js
@@ -1,15 +1,26 @@
-function walkDog() {
+function walkDog(callback) {
   setTimeout(() => {
     console.log("You walk the Dog");
+    callback();
   }, 1500);
 }
-function cleanKitchen() {
+function cleanKitchen(callback) {
   setTimeout(() => {
     console.log("You clean the kitchen");
+    callback();
   }, 1500);
 }
-function takeOutTrash() {
+function takeOutTrash(callback) {
   setTimeout(() => {
     console.log("Take out the trash");
+    callback();
   }, 2500);
 }
+
+walkDog(() => {
+  cleanKitchen(() => {
+    takeOutTrash(() => {
+      console.log("You finish al tasks");
+    });
+  });
+});
