4438f241c38a1f8411b50760761d1a3456dfecb1
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-30 17:45:48 +0530

    commit

--- inex.js
+++ inex.js
@@ -1,3 +1,18 @@
 function task1() {
-  console.log("Tast Complete");
+  console.log("Tast 1 Complete");
 }
+function task2() {
+  console.log("Tast 2 Complete");
+}
+function task3() {
+  console.log("Tast 3 Complete");
+}
+function task4() {
+  console.log("Tast 4 Complete");
+}
+
+task1();
+task2();
+task3();
+task4();
+console.log("all tasks comple");
