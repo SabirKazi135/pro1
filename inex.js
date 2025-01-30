809fa4e1e6f4c2294320f3d15e3c5360aa06dbbf
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-30 22:19:10 +0530

    commit

--- inex.js
+++ inex.js
@@ -2,42 +2,37 @@ function walkDog() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("You walk the Dog");
-    }, 1500);
+    }, 1500); // ✅ Waits 1.5 seconds
   });
 }
+
 function cleanKitchen() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("You clean the kitchen");
-    });
-  }, 500);
+    }, 500); // ✅ Waits 0.5 seconds
+  });
 }
+
 function takeOutTrash() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("Take out the trash");
-    });
-  }, 10000);
+    }, 10000); // ✅ Waits 10 seconds
+  });
 }
 
-// walkDog(() => {
-//   cleanKitchen(() => {
-//     takeOutTrash(() => {
-//       console.log("You finish all tasks");
-//     });
-//   });
-// });
-
+// Running the tasks in order
 walkDog()
   .then((value) => {
     console.log(value);
-    return cleanKitchen();
+    return cleanKitchen(); // ✅ Waits before moving to the next task
   })
   .then((value) => {
     console.log(value);
-    return takeOutTrash();
+    return takeOutTrash(); // ✅ Waits 10 seconds before continuing
   })
   .then((value) => {
     console.log(value);
-    console.log("You Finished all the chores");
+    console.log("You Finished all the chores"); // ✅ Runs after all tasks are done
   });
