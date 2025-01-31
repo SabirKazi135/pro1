3e1d9ef157a40cdcbf6254bb961b890ff3f55ca3
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-31 19:18:33 +0530

    commit

--- inex.js
+++ inex.js
@@ -1,2 +1,9 @@
-let order = () => {};
-let production = () => {};
+let order = (call_production) => {
+  console.log("Order placed, please call production");
+  call_production();
+};
+let production = () => {
+  console.log("Order received, starting production");
+};
+
+order(production);
