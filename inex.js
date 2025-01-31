046402ed43be66a67a24637ab43de7aaeec51efc
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-31 19:16:46 +0530

    commit

--- inex.js
+++ inex.js
@@ -1,9 +1,2 @@
-function one(call_two) {
-  call_two();
-  console.log("step 1 complete. pese call step 2");
-}
-function two() {
-  console.log("step 2");
-}
-
-one(two);
+let order = () => {};
+let production = () => {};
