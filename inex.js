a2a043a332a69bf0eb5949dbf83b8005d271d54f
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-31 19:22:14 +0530

    commit

--- inex.js
+++ inex.js
@@ -1,5 +1,8 @@
-let stockts = {
-  fruits: ["strawberry", "grapes", "banana", "apple"],
+let stocks = {
+  Fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
 };
 
 let order = (call_production) => {
