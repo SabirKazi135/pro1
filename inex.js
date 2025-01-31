4d4ac1fd8c88f0f6c26e5a30d948a1ffb2ff8395
SabirKazi135 <sabirkazi135@gmail.com>
2025-01-31 15:12:44 +0530

    commit

--- inex.js
+++ inex.js
@@ -1,5 +1,3 @@
-fetch("./names.json")
-  .then((response) => response.json())
-  .then((value) => {
-    console.log(value.users);
-  });
+document.cookie =
+  "firstName=sabir; expires=Sun, 01 Jan 2030 12:00:00 GMT; path=/";
+console.log(document.cookie);
