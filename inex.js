62afc09a186bb1f9c25e290a29d7e3bf8075add7
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-01 09:01:26 +0530

    commit

--- inex.js
+++ inex.js
@@ -3,9 +3,8 @@ fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
     return response.json(); // parse the JSON response
   })
   .then((data) => {
-    console.log(`data <br>${data.name}<br> is here`); // Access the 'name' property of the Pokémon
+    console.log(data); // Access the 'name' property of the Pokémon
   })
   .catch((error) => {
-    console.log("Unable to load");
     console.log(error);
   });
