ae756f0211038ca9de32654040522fb7798fa33f
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-01 08:22:40 +0530

    commit

--- inex.js
+++ inex.js
@@ -1,9 +1,14 @@
 let sayHello = new Promise((resolve, reject) => {
+  reject("Unable to say hi.");
   setTimeout(() => {
     resolve("hi, universe");
   }, 5000);
 });
 
-sayHello.then((msg) => {
-  console.log(msg);
-});
+sayHello
+  .then((msg) => {
+    console.log(msg);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
