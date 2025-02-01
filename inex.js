c4dc2c28224682619dac0d95b29ba53c12ed5e08
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-01 15:49:47 +0530

    commit

--- inex.js
+++ inex.js
@@ -1,56 +1,20 @@
-function wakeUp() {
-  return new Promise((resolve, reject) => {
-    let wakeUpIsTrue = Math.random() < 0.5;
-    if (wakeUpIsTrue) {
+function orderFood() {
+  let isOpen = Math.random() * 0.8;
+  new Promise((resolve, reject) => {
+    if (isOpen) {
       setTimeout(() => {
-        resolve("You wake up!");
-      }, 1000);
+        resolve("Food ordered!");
+      }, 2);
     } else {
-      reject("did not wake up");
+      reject("Retaurant is Closed");
     }
   });
 }
 
-function brushTeeth() {
-  return new Promise((resolve, reject) => {
-    let isBrushTeeth = Math.random() < 0.5;
-    if (isBrushTeeth) {
-      setTimeout(() => {
-        resolve("You brushed your teeth");
-      }, 2000);
-    } else {
-      reject("did not brush");
-    }
+function prepareFood() {
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Food prepared!");
+    }, 5);
   });
 }
-
-function makeBrakeFast() {
-  return new Promise((resolve, reject) => {
-    let isMakeBrakeFast = Math.random() < 0.5;
-    if (isMakeBrakeFast) {
-      setTimeout(() => {
-        resolve("You made breakfast");
-      }, 3000);
-    } else {
-      reject("did not made breakfast");
-    }
-  });
-}
-
-async function wakeupFun() {
-  let msg = await wakeUp().catch((err) => {
-    return "Skipping wake up";
-  });
-  console.log(msg);
-  let msg2 = await brushTeeth().catch((err) => {
-    return "Skipping brushing";
-  });
-  console.log(msg2);
-  let msg3 = await makeBrakeFast().catch((err) => {
-    return "skipping breakfast";
-  });
-  console.log(msg3);
-  // ✅ Will still log all results (successful or failed)
-}
-
-wakeupFun();
