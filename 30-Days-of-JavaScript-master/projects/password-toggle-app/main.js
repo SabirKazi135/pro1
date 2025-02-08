4a24b2ee734e6671d99061c3bb51a46ed7b1beb0
SabirKazi135 <sabirkazi135@gmail.com>
2025-02-08 13:00:30 +0530

    commit

--- 30-Days-of-JavaScript-master/projects/password-toggle-app/main.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('#input');
-const showHideBtn = document.querySelector('.show-hide');
-
-showHideBtn.addEventListener('click', () => {
-    input.type = input.type === 'password' ? 'text' : 'password';
-    showHideBtn.classList.toggle('show');
-    showHideBtn.classList.toggle('hide');
-});
